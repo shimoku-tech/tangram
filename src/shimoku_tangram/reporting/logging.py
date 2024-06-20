f25cbf29cad50acfa3c5a70fff2d8a1aac16e4f7
Guillermo Rivero <guillermo.rivero@shimoku.com>
2024-06-20 15:00:23 +0100

    Add-xray-support

--- src/shimoku_tangram/reporting/logging.py
+++ src/shimoku_tangram/reporting/logging.py
@@ -1,4 +1,17 @@
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 import logging
+
+class XRayFilter(logging.Filter):
+    def filter(self, record):
+        segment = xray_recorder.current_segment()
+        if segment:
+            record.trace_id = segment.trace_id
+            record.segment_id = segment.id
+        else:
+            record.trace_id = None
+            record.segment_id = None
+        return True
 
 def init_logger(name: str, level: int | None = None) -> logging.Logger:
     logger = logging.getLogger(name)
@@ -7,9 +20,16 @@ def init_logger(name: str, level: int | None = None) -> logging.Logger:
     else:
         logger.setLevel(logging.ERROR)
     formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        "%(asctime)s - %(name)s - %(levelname)s - [%(trace_id)s | %(segment_id)s] - %(message)s"
     )
+
     ch = logging.StreamHandler()
     ch.setFormatter(formatter)
+    ch.addFilter(XRayFilter())
+
     logger.addHandler(ch)
+    patch_all()
     return logger
+
+
+
