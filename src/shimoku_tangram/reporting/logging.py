53487c62fb5fcb6cd9834ea7e2381b698ed8446b
Guillermo Rivero <guillermo.rivero@shimoku.com>
2024-07-02 10:56:04 +0100

    Generate traceid from env vars

--- src/shimoku_tangram/reporting/logging.py
+++ src/shimoku_tangram/reporting/logging.py
@@ -1,12 +1,14 @@
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 import logging
+import os
 
 patch_all()
 
 class XRayFilter(logging.Filter):
     def filter(self, record):
         segment = xray_recorder.current_segment()
+        
         if segment:
             record.trace_id = segment.trace_id
             record.segment_id = segment.id
@@ -18,6 +20,15 @@ class XRayFilter(logging.Filter):
 def init_logger(name: str, level: int | None = None) -> logging.Logger:
     logger = logging.getLogger(name)
     segment = xray_recorder.current_segment()
+
+    trace_id = os.getenv("AWS_XRAY_TRACE_ID", None)
+    segment_id = os.getenv("AWS_XRAY_SEGMENT_ID", None)
+
+    if trace_id:
+        if segment_id:
+            xray_recorder.begin_segment(segment_id, traceid=trace_id)
+        else:
+            xray_recorder.begin_segment(name, traceid=trace_id)
 
     if not segment:
         xray_recorder.begin_segment(name)
