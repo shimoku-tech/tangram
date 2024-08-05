dd52a180c28b166f5a88357ea32a70e509e5a881
Guillermo Rivero <guillermo.rivero@shimoku.com>
2024-08-05 17:11:42 +0100

    Allow init logging without xray

--- src/shimoku_tangram/reporting/logging.py
+++ src/shimoku_tangram/reporting/logging.py
@@ -1,37 +1,21 @@
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.core import patch_all
 import logging
-import os
-
-
-patch_all()
 
 class XRayFilter(logging.Filter):
     def filter(self, record):
         segment = xray_recorder.current_segment()
-        
         if segment:
             record.trace_id = segment.trace_id
         else:
             record.trace_id = None
         return True
+    
 
-def init_logger(name: str, level: int | None = None) -> logging.Logger:
+def init_xray_logger(name: str, level: int | None = None) -> logging.Logger:
+    patch_all()
     logger = logging.getLogger(name)
-    segment = None
 
-    trace_id = os.getenv("AWS_XRAY_TRACE_ID", None)
-    segment_id = os.getenv("AWS_XRAY_SEGMENT_ID", None)
-
-    if trace_id:
-        if segment_id:
-            segment = xray_recorder.begin_segment(segment_id, traceid=trace_id)
-        else:
-            segment = xray_recorder.begin_segment(name, traceid=trace_id)
-
-    if not segment:
-        xray_recorder.begin_segment(name)
-        
     if level is not None:
         logger.setLevel(level)
     else:
@@ -44,6 +28,22 @@ def init_logger(name: str, level: int | None = None) -> logging.Logger:
     ch.setFormatter(formatter)
     ch.addFilter(XRayFilter())
 
+    logger.addHandler(ch)
+    return logger
+
+def init_logger(name: str, level: int | None = None) -> logging.Logger:
+    logger = logging.getLogger(name)
+        
+    if level is not None:
+        logger.setLevel(level)
+    else:
+        logger.setLevel(logging.ERROR)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - [ No Trace ] - %(message)s"
+    )
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
 
